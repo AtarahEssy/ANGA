d9d286751786e967799a46384ae5498aa7940d0b
AtarahEssy <155487607+AtarahEssy@users.noreply.github.com>
2024-04-01 21:38:28 +0300

    Created JS classes and functions for the weather data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 function refreshWeather (response){
     let temperatureElement = document.querySelector("#temperature");
-    let temperature=response.data.temperature.current;
+        let temperature=response.data.temperature.current;
     temperatureElement.innerHTML = Math.round(temperature);
+
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
+
+    let windSpeedElement = document.querySelector("#wind-speed");
+    windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
+
+    let descriptionElement= document.querySelector("#description");
+    descriptionElement.innerHTML=response.data.condition.description;
+
     let cityElement=document.querySelector("#city");
     cityElement.innerHTML=response.data.city;
+
+    let date= new Date(response.data.time*1000);
+    let timeElement=document.querySelector("#time");
+    timeElement.innerHTML = formatDate(date);
+
+    
+}
+
+function formatDate(date){
+      let minutes=date.getMinutes();
+    let hours=date.getHours();
+    let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    
+    if (minutes < 10){
+        minutes= `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
 }
 
 
@@ -13,8 +42,6 @@ let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKe
 axios.get(apiUrl).then(refreshWeather);
 }
 
-
-
 function  handleSearchSubmit(event){
     event.preventDefault();
     let searchInput=document.querySelector("#search-form-input");
@@ -23,3 +50,5 @@ function  handleSearchSubmit(event){
 
 let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
