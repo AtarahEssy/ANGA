7b7e3ef85aa24a9f7c510dc81e960359df7eed37
AtarahEssy <155487607+AtarahEssy@users.noreply.github.com>
2024-04-01 20:37:16 +0300

    Added API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function refreshWeather (response){
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature=response.data.temperature.current;
+    temperatureElement.innerHTML = Math.round(temperature);
+    let cityElement=document.querySelector("#city");
+    cityElement.innerHTML=response.data.city;
+}
+
+
+function searchCity(city){
+let apiKey= "9e0abb01a4f2312b4c89d4atfo58c538";
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
 function  handleSearchSubmit(event){
     event.preventDefault();
     let searchInput=document.querySelector("#search-form-input");
-    let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement=document.querySelector("#search-form");
